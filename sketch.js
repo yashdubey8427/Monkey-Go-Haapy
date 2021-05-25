206208c885e9d2c570f43e0fc06ce4e394fd18ad
yashdubey8427 <84786846+yashdubey8427@users.noreply.github.com>
2021-05-25 19:28:27 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,129 @@
+
+var monkey , monkey_running
+var banana ,bananaImage, obstacle, obstacleImage
+var FoodGroup, obstacleGroup
+var score;
+var survivalTime=0;
+
+function preload(){
+  
+  
+  monkey_running =            loadAnimation("sprite_0.png","sprite_1.png","sprite_2.png","sprite_3.png","sprite_4.png","sprite_5.png","sprite_6.png","sprite_7.png","sprite_8.png")
+  
+  bananaImage = loadImage("banana.png");
+  obstaceImage = loadImage("obstacle.png");
+ 
+}
+
+
+
+function setup() {
+  createCanvas (600,600)
+
+  monkey = createSprite (100,500, 20, 20) 
+  monkey.addAnimation("monkey_running", monkey_running);
+  monkey.scale = 0.2;
+  
+  //obstacles = createSprite(300,500,20,20)
+
+  
+  ground = createSprite(100,570,600,20);
+  ground.x = ground.width /2;
+  
+  obsctacleGroup = createGroup();
+  bananaGroup = createGroup();
+  
+  
+  score = 0;
+  
+  
+}
+
+
+function draw() {
+background("green")
+  
+ if (ground.x < 0){
+    ground.x = ground.width/2;
+  }
+  spawnObsctacles();
+  spawnBanana();
+  
+  if(keyDown("space") && monkey.y >=300) {
+       monkey.velocityY = -12;
+  }
+  
+  
+  if (bananaGroup.isTouching(monkey)){
+  bananaGroup.destroyEach();
+  score=score+1
+   }
+  
+  monkey.velocityY = monkey.velocityY+0.8
+  
+  
+  
+  monkey.collide(ground);
+  
+  drawSprites();
+  
+  fill("white")
+  text("Score: "+ score, 500,50);
+  
+  fill("black")
+  textSize(20)
+  survivalTime=Math.ceil(frameCount/frameRate())
+  text("Survival Time: "+ survivalTime,100,50)
+  
+  } 
+
+
+
+function spawnObsctacles(){
+  if (frameCount % 150 === 0){
+    var obsctacles = createSprite(500,530,20,20);
+    obstacle.addImage(obstacleImage);
+   obstacle.velocityX = -6;
+    
+    // generate random obstacles
+    var rand = Math.round(random(1));
+    switch(rand) {
+      case 1: obstacle.addImage(obstacleImage);
+              break;
+              default: break;
+              
+    //assign scale and lifetime to the obstacles
+     obstacle.scale = 0.25;
+    obstacle.lifetime = 500;
+              
+     //add each obstacle to the Group
+       obstacleGroup.add(obstacle);
+              
+              
+  }
+    
+    
+    
+    
+}
+  }
+
+ function spawnBanana() {
+   //write code here to spwan the clouds
+   if (frameCount % 160 === 0){
+     banana = createSprite(600,100,40,10);
+     banana.y = Math.round(random(250,300));
+     banana.scale = 0.2;
+     banana.velocityX = -3; 
+     
+     //assign lifetime to the variable
+     monkey.lifetime = 500;
+     
+     //adjust the depth
+     banana.depth = monkey.depth;
+     monkey.depth = monkey.depth +1;
+     
+     // adding cloud to the group
+     bananaGroup.add(banana);
+   }
+}
